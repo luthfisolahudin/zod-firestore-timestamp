2890a0db0584c5c8e84d72c62920bc2f6fd28491
Luthfi Solahudin <luthfisolahudin@outlook.com>
2026-04-04 19:57:39 +0700

    wip: working proof of concept

--- packages/zod-firestore-timestamp/src/index.ts
+++ packages/zod-firestore-timestamp/src/index.ts
@@ -1,7 +1,41 @@
-import { serverTimestamp } from "firebase/firestore"
-import { FieldValue } from 'firebase-admin/firestore'
+import { FieldValue as ClientFieldValue, serverTimestamp } from "firebase/firestore"
+import { FieldValue as AdminFieldValue } from 'firebase-admin/firestore'
+
+interface InternalClientFieldValue {
+  _methodName: string | 'serverTimestamp'
+}
+
+const clientServerTimestamp = serverTimestamp()
+
+export function isClientSdkServerTimestamp<T extends ClientFieldValue>(field: T): field is T & InternalClientFieldValue {
+  const methodName = (field as unknown as InternalClientFieldValue)._methodName
+
+  return methodName === 'serverTimestamp'
+}
+
+const isClientSdkServerTimestampResult = isClientSdkServerTimestamp(clientServerTimestamp)
+
+interface InternalAdminFieldValue {
+  get methodName(): string | 'FieldValue.serverTimestamp'
+}
+
+const adminFieldValue = AdminFieldValue.serverTimestamp()
+
+export function isAdminSdkServerTimestamp<T extends AdminFieldValue>(field: T): field is T & InternalAdminFieldValue {
+  const methodName = (field as unknown as InternalAdminFieldValue).methodName
+
+  return methodName === 'FieldValue.serverTimestamp'
+}
+
+const isAdminSdkServerTimestampResult = isAdminSdkServerTimestamp(adminFieldValue)
 
 console.dir({
-  clientSDK: serverTimestamp(),
-  adminSDK: FieldValue.serverTimestamp(),
+  clientServerTimestamp,
+  clientSdkMethod: (clientServerTimestamp as any)._methodName,
+  isClientSdkServerTimestampResult,
+
+  adminFieldValue,
+  adminSdkMethod: (adminFieldValue as any).methodName,
+  isAdminSdkServerTimestampResult,
 })
+
